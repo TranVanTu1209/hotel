ee2fbea05d385e56470d929b5905532d7246fe51
TranVanTu1209 <trantugiang122899@gmail.com>
2020-02-29 00:07:24 +0700

    responsive change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+// google map deploy
+  // initialize a map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector('#list-hotels-result aside .hotel-map'), {
+    center: { lat: 34.397, lng: 20.644 },
+    zoom: 10
+  });
+}
+
+// slide down nav-bar
+
+$(".show-bars").on("click",
+  function () {
+    $(".nav-bar").fadeToggle(300);
+});
+
+
+
+  // 
 // filter hotel section tool
